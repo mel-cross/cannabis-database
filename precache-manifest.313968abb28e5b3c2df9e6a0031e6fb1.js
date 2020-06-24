15a0ddd1dc3baa279222a9ad9f61e7606c9ad3c0
Melissa Ahlstrom <melissa.ahlstrom@hotmail.com>
2020-06-24 15:52:05 -0400

    Updates

---
+++ precache-manifest.313968abb28e5b3c2df9e6a0031e6fb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec9ba856260190e59f15e1ac6eada79",
+    "url": "/melissa-ahlstrom-project-five/index.html"
+  },
+  {
+    "revision": "ab8a459ba0abb62ec0e5",
+    "url": "/melissa-ahlstrom-project-five/static/css/main.21b1fc50.chunk.css"
+  },
+  {
+    "revision": "a8d8d318ab48588083be",
+    "url": "/melissa-ahlstrom-project-five/static/js/2.47a84127.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/melissa-ahlstrom-project-five/static/js/2.47a84127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8a459ba0abb62ec0e5",
+    "url": "/melissa-ahlstrom-project-five/static/js/main.af7e0856.chunk.js"
+  },
+  {
+    "revision": "74b8e08d1c99549a99ac",
+    "url": "/melissa-ahlstrom-project-five/static/js/runtime-main.0c33b395.js"
+  },
+  {
+    "revision": "7cc1557e8584722f7d92cae4c838dd4c",
+    "url": "/melissa-ahlstrom-project-five/static/media/header-image.7cc1557e.png"
+  }
+]);
